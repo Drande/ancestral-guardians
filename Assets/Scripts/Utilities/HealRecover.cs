0e53010055bcb6754965ec037bdca07ff840e317
GermanRM <germanrm@gmail.com>
2024-08-29 19:26:54 -0500

    SFX sounds implemented

--- Assets/Scripts/Utilities/HealRecover.cs
+++ Assets/Scripts/Utilities/HealRecover.cs
@@ -4,10 +4,12 @@ public class HealRecover : MonoBehaviour
 {
     [SerializeField] private int fixedAmount;
     [SerializeField][Range(0, 1)] private float percentage;
+    [SerializeField] private string healSfx;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
             if(other.TryGetComponent<Health>(out var health)) {
+                AudioManager.Instance.PlaySFX(healSfx);
                 health.Heal(fixedAmount, percentage);
                 gameObject.SetActive(false);
             }
